25247ef71c06c388c8520137ebe38c7d1a502c28
aakash2820 <aakashsaravanan2007@gmail.com>
2025-10-27 12:44:53 +0530

    commited back connection

---
+++ backend/login.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const { Pool } = require("pg");
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool.connect()
+  .then(() => console.log("✅ Connected to PostgreSQL"))
+  .catch((err) => {
+    console.error("❌ Database connection error:", err.message);
+    process.exit(1);
+  });
