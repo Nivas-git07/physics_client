d905d12a3953ce8c3c273a1cf397cc31b07ee07f
aakash2820 <aakashsaravanan2007@gmail.com>
2025-10-27 15:21:09 +0530

    commiteed n8n

--- backend/login.js
+++ backend/login.js
@@ -4,6 +4,7 @@ const { Pool } = require("pg");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -30,6 +31,40 @@ const JWT_SECRET = "supersecret";
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  const now = new Date();   // this creates a Date object for the current moment :contentReference[oaicite:0]{index=0}
+  const formatted = now.toISOString();
+  
+try{
+   const otpresult= await pool.query( 
+   "INSERT into userotp (email,otp,created_at) VALUES ($1,$2,$3) ",
+   [email,otp,formatted]); 
+    
+   try {
+    await axios.post('https://obito123.app.n8n.cloud/webhook/e9d4048a-a920-4f04-acb8-2211971f6859', {
+      email: email,
+      otp: otp
+    });
+
+    res.json({ message: "OTP sent to n8n successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to send OTP to n8n" });
+  }
+   
+}
+catch(err){
+    console.error("Error inserting OTP:", err.message);
+    res.status(500).json({ error: "Internal server error" });
+    return;
+}
+
+// app.post("/verify-otp", async (req, res) => {
+//     const {email,checkotp,password}=req.body;
+//     if(otp==)
+//         const 
+//     }
+
   try {
     const result = await pool.query(
       "INSERT INTO login (email,password) VALUES ($1,$2) ",
